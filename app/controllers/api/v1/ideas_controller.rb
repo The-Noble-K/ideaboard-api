327ae8b32c44509b831345c43ea699401ad8b71f
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-17 00:29:36 -0400

    order ideas

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,7 +2,7 @@ module Api::V1
     class IdeasController < ApplicationController
 
         def index
-            @ideas = Idea.all
+            @ideas = Idea.order("created_at DESC")
             render json: @ideas
         end
 
